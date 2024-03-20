f1715051be3ebd4c7026465e6987014b5980dc04
VoidY <ysoseriousc@gmail.com>
2024-03-20 23:49:52 +0900

    加入环境配置文件

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 # config.py
+import os
+from dotenv import load_dotenv
+# 加载 .env 文件中的环境变量
+load_dotenv()
 class Config:
-    GRAPHQL_URL = "https://leetcode.cn/graphql/"
-    MONGO_URI = "替换成自己的mongoDBURI"
-    MONGO_DB = "leetcode"
-    MONGO_COLLECTION = "daily_questions"
+    GRAPHQL_URL = os.getenv('GRAPHQL_URL')
+    MONGO_URI = os.getenv('MONGO_URI')
+    MONGO_DB = os.getenv('MONGO_DB')
+    MONGO_COLLECTION = os.getenv('MONGO_COLLECTION')
 
 config = Config()
